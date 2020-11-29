f7c66fa6c412e9689763baafad392a9913cac025
TheSlimReaper <55918738+TheSlimReaper925@users.noreply.github.com>
2020-11-29 11:06:07 -0800

    added auth

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,7 @@ Route::delete('/cars/{car}',[
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
